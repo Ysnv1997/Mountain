5f30153751dea977541d01a8e268ca0823fa8ad5
Ysnv <100954636@qq.com>
2020-02-29 23:02:54 +0800

    原本样式1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ var config = {
         notify: false, // 邮件提醒!!! 默认为 false，启动请参考：https://github.com/xCss/Valine/wiki/Valine-%E8%AF%84%E8%AE%BA%E7%B3%BB%E7%BB%9F%E4%B8%AD%E7%9A%84%E9%82%AE%E4%BB%B6%E6%8F%90%E9%86%92%E8%AE%BE%E7%BD%AE
         verify: false //是否开启评论验证码
     },
-    GhostApi: '91c34e137ab452d7836f7fe976'
+    GhostApi: '89c0265e233338dbc72e197be4'
 }
 
 $(document).ready(function() {
@@ -79,6 +79,8 @@ $(document).ready(function() {
         notify: config.Valine.notify,
         verify: config.Valine.verify
     })
+
+
 
     //搜索配置
 
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,48 @@
 *------------------------------------------------------------
 */
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function parseTime(time, cFormat) {
+  if (arguments.length === 0) {
+    return null;
+  }
+
+  var format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}';
+  var date;
+
+  if (_typeof(time) === 'object') {
+    date = time;
+  } else {
+    if (typeof time === 'string' && /^[0-9]+$/.test(time)) {
+      time = parseInt(time);
+    }
+
+    if (typeof time === 'number' && time.toString().length === 10) {
+      time = time * 1000;
+    }
+
+    date = new Date(time);
+  }
+
+  var formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  };
+  return format.replace(/{([ymdhisa])+}/g, function (result, key) {
+    var value = formatObj[key]; // Note: getDay() returns 0 on Sunday
+
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value];
+    }
+
+    return value.toString().padStart(2, '0');
+  });
+}
 ! function ($) {
     "use strict";
 
